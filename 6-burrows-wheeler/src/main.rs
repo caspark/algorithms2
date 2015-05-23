05201f8d28db1fa4212532b2aabb719515e5fc43
Caspar Krieger <caspar@asparck.com>
2015-05-23 17:24:33 +1000

    Working move to front encoding & decoding

--- 6-burrows-wheeler/src/main.rs
+++ 6-burrows-wheeler/src/main.rs
@@ -1,5 +1,38 @@
+extern crate itertools;
+
+use std::env;
+
 mod move_to_front;
 
+// Use for for debugging
+macro_rules! printerrln(
+    ($($arg:tt)*) => (
+        match writeln!(&mut ::std::io::stderr(), $($arg)* ) {
+            Ok(_) => {},
+            Err(x) => panic!("Unable to write to stderr: {}", x),
+        }
+    )
+);
+
 fn main() {
-    println!("Nothing interesting here yet.")
+    let args: Vec<_> = env::args().collect();
+
+    let print_usage = || {
+        let program_name = &args[0];
+        println!("Usage: {} move-to-front [encode|decode]", program_name);
+    };
+
+    if args.len() != 3 {
+        println!("Error: unexpected number of arguments!");
+        print_usage();
+    } else {
+        match (args[1].as_ref(), args[2].as_ref()) {
+            ("move-to-front", "encode") => move_to_front::encode(),
+            ("move-to-front", "decode") => move_to_front::decode(),
+            (operation, command) => {
+                println!("Error: unrecognised arguments: {} {}", operation, command);
+                print_usage();
+            },
+        };
+    }
 }
--- 6-burrows-wheeler/src/move_to_front.rs
+++ 6-burrows-wheeler/src/move_to_front.rs
@@ -1,7 +1,68 @@
-fn encode() {
+use itertools::*;
+use std::io::prelude::*;
+use std::io;
 
+macro_rules! printerrln(
+    ($($arg:tt)*) => (
+        match writeln!(&mut ::std::io::stderr(), $($arg)* ) {
+            Ok(_) => {},
+            Err(x) => panic!("Unable to write to stderr: {}", x),
+        }
+    )
+);
+
+/// Reads from stdin and performs move to front encoding, writing the result to stdout
+pub fn encode() {
+    let stdin = io::stdin();
+    let stdin_lock = stdin.lock();
+    let stdout = io::stdout();
+    let mut stdout_lock = stdout.lock();
+
+    let mut alphabet = (0..u8::max_value()).collect::<Vec<_>>();
+
+    for in_byte in stdin_lock.bytes().map(|r| r.unwrap()) {
+        let byte_pos = alphabet.iter().find_position(|&&a| a == in_byte)
+            .expect("alphabet covers all byte values")
+            .0; // discard the found letter
+        let out_byte = byte_pos as u8;
+        stdout_lock.write(&[out_byte]).unwrap();
+        move_byte_to_front(alphabet.as_mut(), byte_pos);
+    }
 }
 
-fn decode() {
+pub fn decode() {
+    let stdin = io::stdin();
+    let stdin_lock = stdin.lock();
+    let stdout = io::stdout();
+    let mut stdout_lock = stdout.lock();
 
+    let mut alphabet = (0..u8::max_value()).collect::<Vec<_>>();
+
+    for in_byte in stdin_lock.bytes().map(|r| r.unwrap()) {
+        let byte_pos = in_byte as usize;
+        let out_byte = alphabet[byte_pos];
+        stdout_lock.write(&[out_byte]).unwrap();
+        move_byte_to_front(alphabet.as_mut(), byte_pos);
+    }
+}
+
+fn move_byte_to_front(slice: &mut [u8], pos: usize) {
+    if pos != 0 {
+        for i in (1..(pos + 1)).rev() { // iterate from pos to 1
+            slice.swap(i, i - 1);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::move_byte_to_front;
+
+    #[test]
+    fn alphabet_is_updated_properly() {
+        println!("breaking loose");
+        let mut vec = vec![0, 1, 2, 3, 4, 5];
+        move_byte_to_front(vec.as_mut(), 3);
+        assert_eq!(vec, vec![3, 0, 1, 2, 4, 5]);
+    }
 }
